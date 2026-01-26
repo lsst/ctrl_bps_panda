f732a050b2e9dba9b8a44d7424e03c78efa406f6
zhaoyu <zhaoyu@desy.de>
2026-01-26 09:52:46 -0500

    Support memory boost and prmon for build task

--- python/lsst/ctrl/bps/panda/utils.py
+++ python/lsst/ctrl/bps/panda/utils.py
@@ -1110,8 +1110,12 @@ def create_idds_build_workflow(**kwargs):
     task_site = get_task_parameter(config, remote_build, "computeSite")
     task_queue = get_task_parameter(config, remote_build, "queue")
     task_rss = get_task_parameter(config, remote_build, "requestMemory")
+    memory_multiplier = get_task_parameter(config, remote_build, "memoryMultiplier")
+    task_rss_retry_step = task_rss * memory_multiplier if memory_multiplier else 0
+    task_rss_retry_offset = 0 if task_rss_retry_step else task_rss
     nretries = get_task_parameter(config, remote_build, "numberOfRetries")
     processing_type = get_task_parameter(config, remote_build, "processingType")
+    priority = get_task_parameter(config, remote_build, "priority")
     _LOG.info("requestMemory: %s", task_rss)
     _LOG.info("Site: %s", task_site)
     # _LOG.info("executable: %s", executable)
@@ -1137,8 +1141,11 @@ def create_idds_build_workflow(**kwargs):
             "value": "log.tgz",
         },
         task_rss=task_rss if task_rss else PANDA_DEFAULT_RSS,
+        task_rss_retry_offset=task_rss_retry_offset,
+        task_rss_retry_step=task_rss_retry_step,
         task_cloud=task_cloud,
         task_site=task_site,
+        task_priority=int(priority) if priority else PANDA_DEFAULT_PRIORITY,
         maxattempt=nretries if nretries > 0 else PANDA_DEFAULT_MAX_ATTEMPTS,
     )
 
